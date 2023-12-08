1e0c721d90d07ce4319e0a88b02015ed328601ca
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-12-08 21:03:06 +0200

    add static , base templates , inherite product list & detail

--- products/models.py
+++ products/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     tags = TaggableManager()
 
 
-    slug = models.SlugField(blank=True,null=True)
+    slug = models.SlugField(blank=True,null=True,unique=True)
 
     def save(self, *args, **kwargs):
         self.slug =slugify(self.name)
