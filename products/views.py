c20569fab4b5efe2d49f758e41d8db29f449ec5e
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-12-16 18:11:09 +0200

    show product reviews from view

--- products/views.py
+++ products/views.py
@@ -10,3 +10,8 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model=Product
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        return context
+    
