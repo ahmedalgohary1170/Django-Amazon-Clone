aa7329b874f766e3b0473881d51d363807e5c0f7
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-12-16 18:30:11 +0200

    show product images in product detail

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Product,Brand,Review
+from .models import Product,Brand,Review,ProductImages
 
 
 class ProductList(ListView):
@@ -13,5 +13,6 @@ class ProductDetail(DetailView):
     def get_context_data(self, **kwargs) :
         context = super().get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(product=self.get_object())
+        context["images"] = ProductImages.objects.filter(product=self.get_object())
         return context
     
