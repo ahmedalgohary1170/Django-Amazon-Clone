dddf6ca26af975812fee91583bda72019c68d398
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-12-27 08:05:32 +0200

    add product List & Detail , Brand List & Detail APIS

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from .views import ProductDetail,ProductList,BrandList,BrandDetail
-
+from . import api
 
 
 
 urlpatterns=[
     path('brands',BrandList.as_view() ),
     path('brands/<slug:slug>',BrandDetail.as_view()),
-    path('',ProductList.as_view()),
-    path('<slug:slug>',ProductDetail.as_view())
+    path('', ProductList.as_view()),
+    path('<slug:slug>', ProductDetail.as_view()),
+    
+    # api
+    path('api/list' , api.ProductListAPI.as_view()),
+    path('api/list/<int:pk>' , api.ProductDetailAPI.as_view()),
+
+    path('api/Brands' , api.BrandListAPI.as_view()),
+    path('api/Brands/<int:pk>', api.BrandDetailAPI.as_view()),
 ]
--- project/settings.py
+++ project/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # packeges
     'taggit',
+    'rest_framework',
 
+    
+    # your apps
     'products',
 ]
 
