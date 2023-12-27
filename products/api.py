af89246fc0b2bde043228a87abf9a6fe980052c8
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2023-12-27 20:37:09 +0200

    add search , order to list API

--- products/api.py
+++ products/api.py
@@ -9,9 +9,10 @@ from .pagination import Mypagination
 class ProductListAPI(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductListSerializer
-    filter_backends = [DjangoFilterBackend , filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend , filters.SearchFilter,filters.OrderingFilter]
     filterset_fields = ['brand', 'flag']
     search_fields = ['name','subtitle','description']
+    ordering_fields = ['price']
 
 class ProductDetailAPI(generics.RetrieveAPIView):
     queryset = Product.objects.all()
