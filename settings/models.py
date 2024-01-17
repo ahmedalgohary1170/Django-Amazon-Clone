adf9820e6429283ca123468523cdbaa400517df7
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-01-17 21:50:21 +0200

    design settings app models

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Settings(models.Model):
+
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='settings')
+    subtitle = models.TextField(max_length = 500)
+    call_us = models.CharField(max_length=25)
+    email_us = models.CharField(max_length=40)
+    emails = models.TextField(max_length=100)
+    phones = models.TextField(max_length=50)
+    address = models.TextField(max_length=100)
+    android_app = models.URLField(blank=True, null=True)
+    ios_app = models.URLField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    youtube = models.URLField(blank=True, null=True)
+
+
+    def __str__(self) :
+        return self.name
