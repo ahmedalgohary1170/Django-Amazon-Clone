00af45458fd03d737d21f5f1a5208e64041b2204
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-02-25 06:33:11 +0200

    design orders & cart & coupon models

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,6 @@ ADDRESS_TYPE=(
 )
 
 class Address(models.Model):
-    user = models.ForeignKey(User,related_name='',on_delete = models.CASCADE)
+    user = models.ForeignKey(User,related_name='user_address',on_delete = models.CASCADE)
     address = models.TextField(max_length = 200)
     type = models.CharField(max_length=12,choices=ADDRESS_TYPE )
--- orders/models.py
+++ orders/models.py
@@ -21,7 +21,7 @@ ORDER_STATUS = (
 class Order(models.Model):
     user = models.ForeignKey(User,related_name='order_owner',on_delete=models.SET_NULL,blank=True, null=True)
     status = models.CharField( choices=ORDER_STATUS,max_length=20)
-    code = models.CharField(default=generate_code)
+    code = models.CharField(default=generate_code , max_length=20)
     order_time = models.DateTimeField(default = timezone.now)
     delivary_time = models.DateTimeField(blank=True, null=True)
     delivary_address = models.ForeignKey(Address,related_name='delivary_address',on_delete=models.SET_NULL,blank=True, null=True)
@@ -35,6 +35,28 @@ class OrderDetail(models.Model):
     quantity = models.IntegerField()
     price = models.FloatField()
     total = models.FloatField()
+
+
+
+CART_STATUS = (
+    ('Inprogress','Inprogress'),
+    ('Completed','Completed'),
+
+)
+
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,related_name='cart_owner',on_delete=models.SET_NULL,blank=True, null=True)
+    status = models.CharField( choices=CART_STATUS,max_length=20)
+    coupon = models.ForeignKey('Coupon',related_name='cart_coupon',on_delete=models.SET_NULL,blank=True, null=True)
+    total_with_coupon = models.FloatField(blank=True, null=True)
+
+class CartDetail(models.Model):
+    order = models.ForeignKey(Cart,related_name = 'cart_detail',on_delete = models.CASCADE)
+    product = models.ForeignKey(Product,related_name = 'cartdetail_product',on_delete = models.SET_NULL,blank=True, null=True)
+    quantity = models.IntegerField(default=1)
+    total = models.FloatField(blank=True, null=True)
 
 
 class Coupon(models.Model):
