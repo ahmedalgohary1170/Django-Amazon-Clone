0bf057f28be173d39ae5c8759a0f6d414437e544
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-02-27 00:10:54 +0200

    calculate cart total

--- orders/models.py
+++ orders/models.py
@@ -52,6 +52,13 @@ class Cart(models.Model):
     coupon = models.ForeignKey('Coupon',related_name='cart_coupon',on_delete=models.SET_NULL,blank=True, null=True)
     total_with_coupon = models.FloatField(blank=True, null=True)
 
+    @property
+    def cart_total(self):
+        total = 0
+        for item in self.cart_detail.all():
+            total += item.total
+        return round(total,2)
+
 class CartDetail(models.Model):
     cart = models.ForeignKey(Cart,related_name = 'cart_detail',on_delete = models.CASCADE)
     product = models.ForeignKey(Product,related_name = 'cartdetail_product',on_delete = models.SET_NULL,blank=True, null=True)
