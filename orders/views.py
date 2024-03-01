9c37ccdb4d0f65981c4aaf176d2e4a018c57125f
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-03-01 07:42:17 +0200

    show data to checkout page

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render,redirect
 from .models import Order,OrderDetail,Cart,CartDetail,Coupon
 from products.models import Product
+from settings.models import DeliveryFee
 def order_list(request):
     data = Order.objects.filter(user=request.user)
     return render(request,'orders/order_list.html',{'orders':data})
 
 
 def checkout(request):
+    cart = Cart.objects.get(user=request.user,status='Inprogress')
+    deliveryFee = DeliveryFee.objects.last().fee
+    cart_detail = CartDetail.objects.filter(cart=cart)
+    sub_total = cart.cart_total
+    discount = 0
+    total = sub_total + deliveryFee
+    return render(request,'orders/checkout.html',{
+        'cart_detail':cart_detail,
+        'deliveryFee':deliveryFee,
+        'sub_total':sub_total,
+        'discount':discount,
+        'total':total,
 
-    return render(request,'orders/checkout.html',{})
+    })
 
 
 def add_to_cart(request):
