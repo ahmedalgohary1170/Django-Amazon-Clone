6d2c836dfed4435ad22c185941ba06eb2b7b52fd
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-03-15 13:48:58 +0200

    add profile models & add signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Address
+from .models import Address , Profile,ContectNumber
 
 admin.site.register(Address)
+admin.site.register(Profile)
+admin.site.register(ContectNumber)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
+def signup(request):
+    pass
+
+
+
+
+
+
+
+
+def user_activate(request):
+    pass
