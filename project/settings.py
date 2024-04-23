070e13cac53b4bf6ce70eabbee5ee54aa1c0f0ae
ahmed_algohary <ahmedalgohary1170@gmail.com>
2024-04-23 18:33:10 +0200

    add jwt token

--- project/settings.py
+++ project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "django_bootstrap5",
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
 
     
     # your apps
@@ -61,7 +62,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
       'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
       ]
 }
 
--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,10 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 
@@ -51,6 +55,9 @@ urlpatterns = [
     
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
 
 
